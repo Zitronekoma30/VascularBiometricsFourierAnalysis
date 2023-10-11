3db008cca99500694499f93417b9dd884f677b37
Leon <leon.andrassik@gmail.com>
2023-10-11 16:14:59 +0200

    nix

---
+++ sort.py
@@ -0,0 +1,15 @@
+PATH_REAL = ''
+PATH_FAKE = ''
+PATH_SORT = ''
+
+def populate_initial(path_real, path_fake):
+    real = []
+    fake = []
+    # populate lists
+    return real, fake
+
+def main(paths_control, path_sort):
+    real, fake = populate_initial(paths_control[0], paths_control[1])
+
+if __name__ == "__main__":
+    main((PATH_REAL,PATH_FAKE), PATH_SORT)
