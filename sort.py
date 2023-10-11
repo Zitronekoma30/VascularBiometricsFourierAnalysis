9f5b6e7b3f4b7c8254dd1d5b11ea7b783c55a69a
Leon <leon.andrassik@gmail.com>
2023-10-11 16:29:45 +0200

    added basic functions for converting and comparing

--- sort.py
+++ sort.py
@@ -1,15 +1,36 @@
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import os
+
 PATH_REAL = ''
 PATH_FAKE = ''
 PATH_SORT = ''
 
-def populate_initial(path_real, path_fake):
-    real = []
-    fake = []
-    # populate lists
-    return real, fake
+def convert_all_img_dir(path):
+    out = []
+    for filename in os.listdir(path):
+        out.append(mpimg.imread(os.path.join(path, filename)))
 
-def main(paths_control, path_sort):
-    real, fake = populate_initial(paths_control[0], paths_control[1])
+def get_image_diff(img1, img2):
+        return np.abs(img1) - np.abs(img2)
+
+def populate_initial(path_real, path_fake, path_sort):
+    # populate lists
+    real = convert_all_img_dir(path_real)
+    fake = convert_all_img_dir(path_fake)
+    sort = convert_all_img_dir(path_sort)
+    return real, fake, sort
+
+def main(real, fake, sort):
+    # TODO implement sorting into either real or fake from sort
+    # depending on whether a real image or a fake image is most similar.
+
+    # TODO implement reversing the process, using the now sorted "sort images"
+    # for reference and instead sorting the real and fake images.
+    # how many real / fake images end up in the correct category will determine
+    # how good / consistent the algorithm is. 
+    pass
 
 if __name__ == "__main__":
-    main((PATH_REAL,PATH_FAKE), PATH_SORT)
+    main(*populate_initial(PATH_REAL, PATH_FAKE, PATH_SORT))
